d37573b95cb9fbab6613e874d5ec4b583f76b3fd
Daniel Feng <d.feng@processand.com>
2019-10-16 17:34:14 +0800

    access cassandra db

--- app/controller/cal.ts
+++ app/controller/cal.ts
@@ -1,7 +1,8 @@
 import { Controller } from 'egg';
+import { Client } from 'cassandra-driver';
 
 export default class CalController extends Controller {
-  public async add() {
+  add() {
     const { ctx, app } = this;
     const result = ctx.service.cal.add(parseInt(ctx.query.one, 10), parseInt(ctx.query.two, 10));
     ctx.logger.info('1 add page: ' + result);
@@ -12,15 +13,31 @@ export default class CalController extends Controller {
     ctx.body = `Hi this is add page, the answer of ${ctx.query.one} and ${ctx.query.two} is ${result}`;
   }
 
-  async sub() {
+  sub() {
     const { ctx } = this;
     const result = ctx.service.cal.sub(parseInt(ctx.query.one, 10), parseInt(ctx.query.two, 10));
     ctx.body = `Hello this is Subtraction page, the answer of ${ctx.query.one} and ${ctx.query.two} is ${result}`;
   }
 
-  async mul() {
+  mul() {
     const { ctx } = this;
     const result = ctx.service.cal.mul(parseInt(ctx.query.one, 10), parseInt(ctx.query.two, 10));
     ctx.body = `你好 this is multiply page, the answer of ${ctx.query.one} and ${ctx.query.two} is ${result}`;
+  }
+
+  async query() {
+    const { ctx, app } = this;
+    const db = new Client({ contactPoints: [ '127.0.0.1' ], localDataCenter: 'datacenter1', keyspace: 'mycasdb' });
+    const query = 'SELECT * FROM user';
+    const dbresult = await db.execute(query);
+    app.logger.info(dbresult);
+    let field = '';
+
+    dbresult.rows.map(content => {
+      app.logger.info(content);
+      field += `id: ${content.id} user_name: ${content.user_name}`;
+    });
+
+    ctx.body = `query content: ${field}`;
   }
 }
