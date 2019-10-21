68909e8fa6eff71bc7aa060f592fb1ffdf4f95a9
Daniel Feng <d.feng@processand.com>
2019-10-21 09:32:53 +0800

    modify test patten

--- test/app/controller/home.test.ts
+++ test/app/controller/home.test.ts
@@ -2,8 +2,8 @@ import * as assert from 'assert';
 import { app } from 'egg-mock/bootstrap';
 
 describe('test/app/controller/home.test.ts', () => {
-  it('should GET /', async () => {
-    const result = await app.httpRequest().get('/').expect(200);
-    assert(result.text === 'hi, egg');
+  it('should GET /home', async () => {
+    const result = await app.httpRequest().get('/home').expect(200);
+    assert(result.text === 'hi, egg test home\ndaniel say:I am the king of the world!');
   });
 });
